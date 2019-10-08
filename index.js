f351090ebe7ac7da9f9ffc6fbea823923b0bcb1f
Brian Simonton <bsimonton78@gmail.com>
2019-10-08 01:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,14 +16,10 @@ const tutorials = [
 //}
 
 
-const titleCased = tutorials.map((tutorial) => {
-	tutorial = tutorial.split(' ');
-	for (var i = 0; i < tutorial.length; i++) {
-		tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
-		console.log(tutorial[i]);
-	}
-	console.log(tutorial);
-	return tutorial.join(' ');
+const titleCased = tutorials.map(function (tutorial) {
+  tutorial = tutorial.split(' ');
+  for (var i = 0; i < tutorial.length; i++) {
+    tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+  }
+  return tutorial.join(' ');
 });
-
-titleCased;
